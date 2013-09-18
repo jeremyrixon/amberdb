8abf5424cc4d848a324ea0d072bc6312dcde76d2
scoen <scoen@nla.gov.au>
2013-09-19 00:18:18 +1000

    refactor to hold transactions in memory db

---
+++ src/amberdb/sql/map/AmberTransactionMapper.java
@@ -0,0 +1,19 @@
+package amberdb.sql.map;
+
+import amberdb.sql.AmberTransaction;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+public class AmberTransactionMapper implements ResultSetMapper<AmberTransaction> {
+    public AmberTransaction map(int index, ResultSet rs, StatementContext ctx)
+            throws SQLException {
+        return new AmberTransaction(
+                rs.getLong("id"), 
+                rs.getString("user"),
+                rs.getDate("commit"));
+    }
+}
---
+++ src/amberdb/sql/map/AmberVertexMapper.java
@@ -0,0 +1,21 @@
+package amberdb.sql.map;
+
+import amberdb.sql.AmberVertex;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+public class AmberVertexMapper implements ResultSetMapper<AmberVertex> {
+    public AmberVertex map(int index, ResultSet rs, StatementContext ctx) 
+            throws SQLException {
+        return new AmberVertex(
+                rs.getLong("id"),
+                rs.getLong("txn_start"),
+                rs.getLong("txn_end"),
+                rs.getString("properties"),
+                rs.getString("pi"));
+    }
+}
