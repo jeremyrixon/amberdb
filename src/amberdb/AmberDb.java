56a9e7d60407631fa70667e0a6404a2194d21b34
Shuang Zhou <szhou@nla.gov.au>
2013-09-20 18:10:50 +1000

    updated ingest apis.

--- src/amberdb/AmberDb.java
+++ src/amberdb/AmberDb.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import amberdb.model.Copy;
 import amberdb.model.File;
+import amberdb.model.Page;
+import amberdb.model.Section;
 import amberdb.model.Work;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 import com.tinkerpop.frames.FramedGraph;
@@ -42,6 +42,20 @@ public class AmberDb implements AutoCloseable {
 			// first run
 		}
 	}
+	
+	/**
+	 * commit saves everything in the current transaction.
+	 */
+	public void commit() {
+	    //TODO: saves everything in the current transaction. 
+	}
+	
+	/**
+     * rollback rollback everything in the current transaction.
+     */
+    public void rollback() {
+        //TODO: saves everything in the current transaction. 
+    }
 
 	protected static FramedGraph<TinkerGraph> openGraph(TinkerGraph tinker) {
 		return new FramedGraphFactory(
@@ -56,6 +70,21 @@ public class AmberDb implements AutoCloseable {
 	public Work findWork(long objectId) {
 		return graph.getVertex(objectId, Work.class);
 	}
+	
+	/**
+     * Finds a section by id.
+     */
+    public Section findSection(long objectId) {
+        return graph.getVertex(objectId, Section.class);
+    }
+    
+    /**
+     * Finds a section by Bib id.
+     */
+    public Section findSectionByVn(long vnLink) {
+        // TODO: to be changed later
+        return graph.getVertex(vnLink, Section.class);
+    }
 
 	/**
 	 * Creates a new work.
@@ -65,7 +94,105 @@ public class AmberDb implements AutoCloseable {
 	public Work addWork() {
 		return graph.addVertex(objectIdSeq.next(), Work.class);
 	}
+	
+	/**
+     * Create a new section.
+     */
+    public Section addSection() {
+        return graph.addVertex(objectIdSeq.next(), Section.class);
+    }
+    
+    /**
+     * Todo: this need to be removed later.
+     * Create a new section.
+     */
+    public Section addSection(long vnNumber) {
+        return graph.addVertex(vnNumber, Section.class);
+    }
+    
+    /**
+     * Create a new section within the input work.
+     * 
+     * @return
+     */
+    public Section addSectionTo(Work work) {
+        Section section = graph.addVertex(objectIdSeq.next(), Section.class);
+        work.addChild(section);
+        return section;
+    }
+    
+	/**
+	 * Create a new page.
+	 * 
+	 * @return
+	 */
+	public Page addPageTo(Section section) {
+	    Page page = graph.addVertex(objectIdSeq.next(), Page.class);
+	    section.addChild(page);
+	    return page;
+	}
+	
+	/**
+	 * Create a new page item for the input file.
+	 * @param file
+	 * @return
+	 */
+	public Page addPageTo(Section section, java.io.File file) {
+	    Page page = addPageTo(section);
+	    addCopyTo(page, file, Copy.Role.MASTER_COPY.code());
+	    return page;
+	}
 
+	/**
+     * Create a new image tiff master copy
+     * 
+     * Note: most often the work is a page, but it doesn't have to be a page,
+     *       it can be book, or other groupItem as well.
+     */
+    public void addImageTiffCopyTo(Work work, java.io.File file) {
+        addCopyTo(work, file, Copy.Role.MASTER_COPY.code());
+    }
+    
+    /**
+     * Create a new image jp2 access copy
+     * 
+     * Note: most often the work is a page, but it doesn't have to be a page,
+     *       it can be book, or other groupItem as well.
+     */
+    public void addImageJP2CopyTo(Work work, java.io.File file) {
+        addCopyTo(work, file, Copy.Role.ACCESS_COPY.code());
+    }
+    
+    /**
+     * Create a new image ocr mets master copy
+     * 
+     * Note: most often the work is a page, but it doesn't have to be a page,
+     *       it can be book, or other groupItem as well.
+     */
+    public void addOCRMETSCopyTo(Work work, java.io.File file) {
+        addCopyTo(work, file, Copy.Role.OCR_METS_COPY.code());
+    }
+
+    /**
+     * Create a new image ocr alto master copy
+     * 
+     * Note: most often the work is a page, but it doesn't have to be a page,
+     *       it can be book, or other groupItem as well.
+     */
+    public void addOCRALTOCopyTo(Work work, java.io.File file) {
+        addCopyTo(work, file, Copy.Role.OCR_ALTO_COPY.code());
+    }
+    
+    /**
+     * Create a new image ocr json derivative copy
+     * 
+     * Note: most often the work is a page, but it doesn't have to be a page,
+     *       it can be book, or other groupItem as well.
+     */
+    public void addOCRJSONCopyTo(Work work, java.io.File file) {
+        addCopyTo(work, file, Copy.Role.OCR_JSON_COPY.code());
+    }
+    
 	@Override
 	public void close() throws IOException {
 		graph.shutdown();
@@ -89,37 +216,36 @@ public class AmberDb implements AutoCloseable {
 	 */
 	public void recover(int txId) {
 	    // TODO
-	}
-	
-	/**
-	 * Create a new copy
-	 */
-	protected Copy addCopy() {
-	    return graph.addVertex(objectIdSeq.next(), Copy.class);
-	}
-	
-	/**
-	 * Create a new copy with specified metadata
-	 */
-	protected Copy addCopy(String copyRole) {
-	    Copy copy = addCopy();
-	    copy.setCopyRole(copyRole);
-	    return copy;
-	}
+	}	
+
+    /**
+     * Create a new copy
+     * @param work - the work to add the copy to.
+     * @param file - the file to attach to the copy.
+     * @param copyRole - the type of the copy.  e.g. master, access copy, etc.
+     */
+    protected void addCopyTo(Work work, java.io.File file, String copyRole) {
+        if (work == null)
+            throw new IllegalArgumentException("Cannot add copy as input work is null.");
+        
+        if (file == null)
+            throw new IllegalArgumentException("Cannot add copy as input file is null.");
+        
+        Copy copy = graph.addVertex(objectIdSeq.next(), Copy.class);
+        copy.setCopyRole(copyRole);
+        work.addCopy(copy);
+        
+        File _file = addFileTo(copy);
+        _file.setLocation(file.getAbsolutePath());
+        copy.addFile(_file);
+    }
 	
 	/**
      * Create a new file
      */
-    protected File addFile() {
-        return graph.addVertex(objectIdSeq.next(), File.class);
-    }
-    
-    /**
-     * Creates a new file with specified metadata
-     */
-    public File addFile(java.io.File inFile) {
-        File file = addFile();
-        file.setFileLocation(inFile.getAbsolutePath());
+    protected File addFileTo(Copy copy) {
+        File file = graph.addVertex(objectIdSeq.next(), File.class);
+        copy.addFile(file);
         return file;
     }
 
---
+++ test/amberdb/IngestTest.java
@@ -0,0 +1,172 @@
+package amberdb;
+
+import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import amberdb.model.Page;
+import amberdb.model.Section;
+
+public class IngestTest {
+    private static AmberDb dao;
+    private static JobMockup job;
+    
+    @BeforeClass
+    public static void setup() throws IOException {
+        job = uploadFiles();
+        dao = identifyWorks();
+    }
+    
+    @AfterClass
+    public static void teardown() {
+        job = null;
+        dao = null;
+    }
+    
+    private static JobMockup uploadFiles() {
+        job = new JobMockup();
+        
+        // Note: banjo upload files step does not interact with amberDb at all.
+        List<File> list = new ArrayList<File>();
+        list.add(new File("/tmp/a.tiff"));
+        list.add(new File("/tmp/b.tiff"));
+
+        list.add(new File("/tmp/nla.aus-vn12345-1.tiff"));
+        list.add(new File("/tmp/nla.aus-vn12345-2.tiff"));
+        list.add(new File("/tmp/nla.aus-vn12345.xml"));
+
+        list.add(new File("/tmp/nla.aus-vn643643-1.tiff"));
+        list.add(new File("/tmp/nla.aus-vn643643-2.tiff"));
+
+        job.files = list;
+        return job;
+    }
+    
+    private static AmberDb identifyWorks() throws IOException {
+        try (AmberDb amberDb = new AmberDb()) {
+
+            // recover existing transaction if any
+            // NOTE: this recover and the corresponding suspend
+            // could be done in a common pre-action and post-action
+            // controller method
+            if (job.getAmberTxId() != null) {
+                amberDb.recover(job.getAmberTxId());
+            }
+
+            List<Long> bookIds = new ArrayList<Long>();
+            // try to detect works based on filenames,
+
+            Section auto1 = amberDb.addSection(12345L);
+            // auto1.setBibId(12345L);
+            amberDb.addPageTo(auto1, job.files.get(6));
+
+            Page page = amberDb.addPageTo(auto1);
+
+            amberDb.addImageTiffCopyTo(page, job.files.get(2));
+            amberDb.addOCRMETSCopyTo(page, job.files.get(4));
+
+            amberDb.addPageTo(auto1, job.files.get(3));
+
+            auto1.setTitle("Blinky Bill");
+            bookIds.add(auto1.getId());
+
+
+            Section auto2 = amberDb.addSection();
+            auto2.setBibId(55555);
+            amberDb.addPageTo(auto2, job.files.get(5));
+            auto2.setTitle("James and the giant peach");
+
+            bookIds.add(auto2.getId());
+
+
+            // user manually creates a work out of the first two pages
+
+            Section manual = amberDb.addSection();
+            amberDb.addPageTo(manual, job.files.get(0));
+            amberDb.addPageTo(manual, job.files.get(1));
+            manual.setTitle("Little red riding hood");
+
+            bookIds.add(manual.getId());
+
+            job.workIds = bookIds;
+
+            // save this transaction without committing it
+            // so we can continue manipulating these objects
+            // in another web request
+            job.setAmberTxId(amberDb.suspend());
+            
+            return amberDb;
+        }
+    }
+    
+    @Test
+    public void testDescribeWorks() {
+        // TODO
+    }
+    
+    // @Test
+    @Ignore
+    public void testQAWorks() {
+        // TODO: currently failing
+        //       to be fixed
+        
+        // recover existing transaction if any
+        if (job.getAmberTxId() != null) {
+            dao.recover(job.getAmberTxId());
+        }
+
+        for (Long workId: job.getWorks()) {
+            qaWork(job, dao.findSection(workId));
+        }
+
+        // save this transaction without committing it
+        // so we can continue manipulating these objects
+        // in another web request
+        job.setAmberTxId(dao.suspend());
+    }
+    
+    @Test
+    public void testCompleteJob() {
+        // recover existing transaction if any
+        dao.recover(job.getAmberTxId());
+        dao.commit();
+    }
+    
+    @Test
+    public void testCancelJob() {
+        dao.rollback();
+    }
+    
+    // @Test
+    @Ignore
+    public void testFixLabel() {
+        // TODO: to be fixed.
+        Section section = dao.findSectionByVn(12345L);
+
+        Page page7 = section.getPage(6);
+        page7.setTitle("III");
+
+        dao.commit();
+    }
+   
+    private void qaWork(JobMockup job, Section section) {
+        // fill in default values
+        for (Page p: section.getPages()) {
+            p.setDevice(job.getDefaultDevice());
+            p.setSoftware(job.getDefaultSoftware());
+        }
+
+        // show the qa form
+        section.swapPages(1,2);
+        Page page = section.getPage(1);
+        page.rotate(10);
+        page.crop(100,100,200,200);
+        page.setTitle("IV");
+    }
+}
