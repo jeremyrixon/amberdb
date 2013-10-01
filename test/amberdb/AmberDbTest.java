5025f10503895daa3af07973da7cb9e524db15b1
Alex Osborne <ato@meshy.org>
2013-10-01 10:11:53 +1000

    Admin CLI and first stub of DCM interface

--- test/amberdb/AmberDbTest.java
+++ test/amberdb/AmberDbTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
+import org.apache.tools.ant.util.WorkerAnt;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -12,33 +13,38 @@ import amberdb.model.Work;
 
 public class AmberDbTest {
 
-	@Rule
-	public TemporaryFolder folder = new TemporaryFolder();
-	
-	@Test
-	public void testInMemory() throws IOException {
-		Work w1, w2;
-		try (AmberDb db = new AmberDb()) {
-			w1 = db.addWork();
-		}
-		try (AmberDb db = new AmberDb()) {
-			assertNull("works should not persist", db.findWork(w1.getId()));
-			w2 = db.addWork();
-		}
-		assertEquals("ids should not persist", w1.getId(), w2.getId());
-	}
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
-	@Test
-	public void testPersistence() throws IOException {
-		Work w1, w2;
-		try (AmberDb db = new AmberDb(folder.getRoot().toPath())) {
-			w1 = db.addWork();
-		}
-		try (AmberDb db = new AmberDb(folder.getRoot().toPath())) {
-			assertNotNull(db.findWork(w1.getId()));
-			w2 = db.addWork();
-		}
-		assertNotEquals(w1.getId(), w2.getId());
-	}
+    @Test
+    public void testInMemory() throws IOException {
+        Work w1, w2;
+        try (AmberDb db = new AmberDb()) {
+            w1 = db.addWork();
+        }
+        try (AmberDb db = new AmberDb()) {
+            try {
+                db.findWork(w1.getId());
+                assertTrue("works should not persist", false);
+            } catch (NoSuchObjectException e) {
+                // ok
+            }
+            w2 = db.addWork();
+        }
+        assertEquals("ids should not persist", w1.getId(), w2.getId());
+    }
+
+    @Test
+    public void testPersistence() throws IOException {
+        Work w1, w2;
+        try (AmberDb db = new AmberDb(folder.getRoot().toPath())) {
+            w1 = db.addWork();
+        }
+        try (AmberDb db = new AmberDb(folder.getRoot().toPath())) {
+            assertNotNull(db.findWork(w1.getId()));
+            w2 = db.addWork();
+        }
+        assertNotEquals(w1.getId(), w2.getId());
+    }
 
 }
