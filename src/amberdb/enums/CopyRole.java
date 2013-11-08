fe9eaa032672f9b481b1abdb3a40c7738bc086dd
Huw Rowlands <hrowland@nla.gov.au>
2013-11-08 12:29:23 +1100

    Added CopyRole fromString method
    So that you can go from a little string code to a CopyRole enum. eg. "m" to a MASTER_COPY.

--- src/amberdb/enums/CopyRole.java
+++ src/amberdb/enums/CopyRole.java
@@ -2,13 +2,24 @@ package amberdb.enums;
 
 public enum CopyRole {
     ACCESS_COPY("ac"), MASTER_COPY("m"), OCR_JSON_COPY("oc"), OCR_ALTO_COPY("at"), OCR_METS_COPY("mt");
-    
+
     private String code;
-    
+
     private CopyRole(String code) {
         this.code = code;
     }
-    
+
+    public static CopyRole fromString(String code) {
+        if (code != null) {
+            for (CopyRole cr : CopyRole.values()) {
+                if (code.equalsIgnoreCase(cr.code)) {
+                    return cr;
+                }
+            }
+        }
+        return null;
+    }
+
     public String code() {
         return code;
     }
