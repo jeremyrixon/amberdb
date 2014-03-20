563f61ebc8a6c8923857e6b4a8f6fc0104436959
JonathanShaw <anaximenes@gmail.com>
2014-03-20 12:58:36 +1100

    adding a bunch of work properties

--- src/amberdb/model/Work.java
+++ src/amberdb/model/Work.java
@@ -41,124 +41,125 @@ import com.tinkerpop.frames.modules.typedgraph.TypeValue;
  * Any logical work that is collected or created by the library such as a book,
  * page, map, physical object or sound recording.
  * 
- * A complex digital object may be made up of multiple related works forming
- * a graph. All works in a single digital object belong to a parent-child
- * tree formed by the {@link IsPartOf} relationship.
+ * A complex digital object may be made up of multiple related works forming a
+ * graph. All works in a single digital object belong to a parent-child tree
+ * formed by the {@link IsPartOf} relationship.
  * 
  * @see {@link Copy}
  */
 @TypeValue("Work")
 public interface Work extends Node {
-    @Property ("abstract")
+    @Property("abstract")
     public String getAbstract();
-    
-    @Property ("abstract")
+
+    @Property("abstract")
     public void setAbstract(String aBstract);
-    
+
     @Property("category")
     public String getCategory();
-    
+
     @Property("category")
     public void setCategory(String category);
-    
+
     /* DCM Legacy Data */
     @Property("dcmWorkPid")
     public String getDcmWorkPid();
 
     @Property("dcmWorkPid")
     public void setDcmWorkPid(String dcmWorkPid);
-    
+
     @Property("dcmDateTimeCreated")
     public Date getDcmDateTimeCreated();
 
     @Property("dcmDateTimeCreated")
     public void setDcmDateTimeCreated(Date dcmDateTimeCreated);
-    
+
     @Property("dcmDateTimeUpdated")
     public Date getDcmDateTimeUpdated();
 
     @Property("dcmDateTimeUpdated")
-    public void setDcmDateTimeUpdated(Date dcmDateTimeUpdated);
-    
+    public void setDcmDateTimeUpdated(Date dcmDateTimeUpd);
+
     @Property("dcmRecordCreator")
     public String getDcmRecordCreator();
 
     @Property("dcmRecordCreator")
     public void setDcmRecordCreator(String dcmRecordCreator);
-    
+
     @Property("dcmRecordUpdater")
     public String getDcmRecordUpdater();
 
     @Property("dcmRecordUpdater")
     public void setDcmRecordUpdater(String dcmRecordUpdater);
+
     /* END DCM Legacy Data */
-    
+
     @Property("subUnitType")
     public String getSubUnitType();
 
     @Property("subUnitType")
     public void setSubUnitType(String subUnitType);
-    
+
     @Property("subUnitNo")
     public String getSubUnitNo();
 
     @Property("subUnitNo")
     public void setSubUnitNo(String subUnitNo);
-    
+
     @Property("subType")
     public String getSubType();
 
     @Property("subType")
     public void setSubType(String subType);
-    
+
     @Property("issueDate")
     public Date getIssueDate();
-    
+
     @Property("issueDate")
     public void setIssueDate(Date issueDate);
-        
+
     @Property("collection")
     public String getCollection();
 
     @Property("collection")
     public void setCollection(String collection);
-    
+
     @Property("form")
     public String getForm();
 
     @Property("form")
     public void setForm(String form);
-    
+
     @Property("bibLevel")
     public String getBibLevel();
 
     @Property("bibLevel")
     public void setBibLevel(String bibLevel);
-    
+
     @Property("digitalStatus")
     public String getDigitalStatus();
 
     @Property("digitalStatus")
     public void setDigitalStatus(String digitalStatus);
-    
+
     @Property("digitalStatusDate")
     public Date getDigitalStatusDate();
 
     @Property("digitalStatusDate")
     public void setDigitalStatusDate(Date digitalStatusDate);
-    
+
     @Property("heading")
     public String getHeading();
-    
+
     @Property("heading")
     public void setHeading(String heading);
-    
+
     @Property("subHeadings")
     public String getSubHeadings();
-    
+
     @Property("subHeadings")
     public void setSubHeadings(String subHeadings);
-    
+
     /**
      * Also known as CALLNO
      */
@@ -170,68 +171,157 @@ public interface Work extends Node {
      */
     @Property("holdingNumber")
     public void setHoldingNumber(String holdingNumber);
-    
+
     @Property("issn")
     public String getISSN();
-    
+
     @Property("issn")
     public void setISSN(String issn);
-    
+
     @Property("title")
     public String getTitle();
 
     @Property("title")
     public void setTitle(String title);
-    
+
     @Property("creator")
     public String getCreator();
 
     @Property("creator")
     public void setCreator(String creator);
-    
+
+    @Property("creatorStatement")
+    public String getCreatorStatement();
+
+    @Property("creatorStatement")
+    public void setCreatorStatement(String creatorStatement);
+
     @Property("publisher")
     public String getPublisher();
 
     @Property("publisher")
     public void setPublisher(String publisher);
-    
+
     @Property("recordSource")
     public String getRecordSource();
 
     @Property("recordSource")
     public void setRecordSource(String recordSource);
-    
+
     @Property("copyrightPolicy")
     public String getCopyrightPolicy();
 
     @Property("copyrightPolicy")
     public void setCopyrightPolicy(String copyrightPolicy);
-    
+
     @Property("firstPart")
     public String getFirstPart();
 
     @Property("firstPart")
     public void setFirstPart(String firstPart);
-    
-  
+
+    @Property("sortIndex")
+    public String getSortIndex();
+
+    @Property("sortIndex")
+    public void setSortIndex(String sortIndex);
+
+    @Property("edition")
+    public String getEdition();
+
+    @Property("edition")
+    public void setEdition(String edition);
+
+    @Property("immutable")
+    public String getImmutable();
+
+    @Property("immutable")
+    public void setImmutable(String immutable);
+
+    @Property("startDate")
+    public Date getStartDate();
+
+    @Property("startDate")
+    public void setStartDate(Date startDate);
+
+    @Property("endDate")
+    public Date getEndDate();
+
+    @Property("endDate")
+    public void setEndDate(Date endDate);
+
+    @Property("extent")
+    public String getExtent();
+
+    @Property("extent")
+    public void setExtent(String extent);
+
+    @Property("language")
+    public String getLanguage();
+
+    @Property("language")
+    public void setLanguage(String language);
+
+    @Property("addressee")
+    public String getAddressee();
+
+    @Property("addressee")
+    public void setAddressee(String addressee);
+
+    @Property("childRange")
+    public String getChildRange();
+
+    @Property("childRange")
+    public void setChildRange(String childRange);
+
+    @Property("startChild")
+    public String getStartChild();
+
+    @Property("startChild")
+    public void setStartChild(String startChild);
+
+    @Property("endChild")
+    public String getEndChild();
+
+    @Property("endChild")
+    public void setEndChild(String endChild);
+
+    @Property("encodingLevel")
+    public String getEncodingLevel();
+
+    @Property("encodingLevel")
+    public void setEncodingLevel(String encodingLevel);
+
+    @Property("publicationLevel")
+    public String getPublicationLevel();
+
+    @Property("publicationLevel")
+    public void setPublicationLevel(String publicationLevel);
+
+    @Property("genre")
+    public String getGenre();
+
+    @Property("genre")
+    public void setGenre(String genre);
+
     /**
      * Also known as localsystmno
      */
     @Property("bibId")
     public String getBibId();
-    
+
     /**
      * Also known as localsystmno
      */
     @Property("bibId")
     public void setBibId(String bibId);
-    
+
     /**
      * To be published in the catalogue
      */
     @Property("publicNotes")
     public String getPublicNotes();
-    
+
     /**
      * To be published in the catalogue
      */
@@ -249,23 +339,23 @@ public interface Work extends Node {
 
     @Adjacency(label = IsPartOf.label, direction = Direction.IN)
     public Iterable<Work> getChildren();
-    
+
     @GremlinGroovy("it.inE.has('label', 'isPartOf').outV.loop(3){true}{true}.has('subType', subType.code)")
     public Iterable<Work> getLeafs(@GremlinParam("subType") SubType subType);
-    
+
     @GremlinGroovy("it.inE.has('label', 'isPartOf').outV.loop(3){true}{true}.has('subType', Subtype.fromString(T).in, subTypes)")
     public Iterable<Work> getLeafs(@GremlinParam("subTypes") List<String> subTypes);
-    
+
     @GremlinGroovy("it.inE.has('label', 'isPartOf').outV.has('subType', subType.code)")
     public Iterable<Section> getSections(@GremlinParam("subType") SubType subType);
-    
+
     // TODO: need to test later whether it has any existsOn outE(s)
     @GremlinGroovy("it")
     public Section asSection();
 
     @Adjacency(label = IsCopyOf.label, direction = Direction.IN)
     public void addCopy(final Copy copy);
-    
+
     @Adjacency(label = IsCopyOf.label, direction = Direction.IN)
     public void removeCopy(final Copy copy);
 
@@ -280,20 +370,20 @@ public interface Work extends Node {
 
     @Adjacency(label = IsPartOf.label, direction = Direction.IN)
     public Page addPage();
-    
+
     /**
-     * This method detatches the page from this work, but the page
-     * continues to exist as an orphan.  Use the deletePage method
-     * in AmberSessoin to actually delete the page with copies and 
-     * files from the graph.
+     * This method detatches the page from this work, but the page continues to
+     * exist as an orphan. Use the deletePage method in AmberSessoin to actually
+     * delete the page with copies and files from the graph.
+     * 
      * @param page
      * 
-     * Note: remove is a naming convention used by tinkerpop frames
-     *       annotation.
+     *            Note: remove is a naming convention used by tinkerpop frames
+     *            annotation.
      */
     @Adjacency(label = IsPartOf.label, direction = Direction.IN)
     public void removePage(final Page page);
-    
+
     @Adjacency(label = IsCopyOf.label, direction = Direction.IN)
     public Copy addCopy();
 
@@ -310,18 +400,20 @@ public interface Work extends Node {
     public Copy addCopy(Path sourceFile, CopyRole copyRole, String mimeType) throws IOException;
 
     @JavaHandler
-    public Copy addLegacyDossCopy(Path dossPath, CopyRole copyRole, String mimeType) throws IOException;
+    public Copy addLegacyDossCopy(Path dossPath, CopyRole copyRole, String mimeType)
+            throws IOException;
 
     @JavaHandler
     public Iterable<Page> getPages();
 
     @JavaHandler
     public int countParts();
-    
+
     /**
-     * This method detaches the part from this work, but the part
-     * continues to exist as an orphan.  Use the deletePart method
-     * to actually delete the part and its children from the graph.
+     * This method detaches the part from this work, but the part continues to
+     * exist as an orphan. Use the deletePart method to actually delete the part
+     * and its children from the graph.
+     * 
      * @param work
      */
     @Adjacency(label = IsPartOf.label, direction = Direction.IN)
@@ -329,10 +421,10 @@ public interface Work extends Node {
 
     @JavaHandler
     public Page getPage(int position);
-    
+
     @JavaHandler
     public Work getLeaf(SubType subType, int position);
-    
+
     @JavaHandler
     public void loadPagedWork() throws InvalidSubtypeException;
 
@@ -347,11 +439,10 @@ public interface Work extends Node {
 
     @JavaHandler
     public List<Work> getExistsOn(String subType);
-    
+
     @JavaHandler
     public AmberGraph getAmberGraph();
 
-    
     abstract class Impl implements JavaHandlerContext<Vertex>, Work {
 
         @Override
@@ -375,15 +466,16 @@ public interface Work extends Node {
             copy.addFile(sourceFile, mimeType);
             return copy;
         }
-        
+
         @Override
-        public Copy addLegacyDossCopy(Path dossPath, CopyRole copyRole, String mimeType) throws IOException {
+        public Copy addLegacyDossCopy(Path dossPath, CopyRole copyRole, String mimeType)
+                throws IOException {
             Copy copy = addCopy();
             copy.setCopyRole(copyRole.code());
             copy.addLegacyDossFile(dossPath, mimeType);
             return copy;
         }
-        
+
         @Override
         public List<Page> getPages() {
             List<Page> pages = new ArrayList<Page>();
@@ -420,18 +512,18 @@ public interface Work extends Node {
             }
             return page;
         }
-               
+
         @Override
         public Work getLeaf(SubType subType, int position) {
             if (position <= 0)
                 throw new IllegalArgumentException("Cannot get this page, invalid input position "
                         + position);
-            
+
             Iterable<Work> leafs = getLeafs(subType);
             if (leafs == null)
                 throw new IllegalArgumentException("Cannot get this page, page at position "
                         + position + " does not exist.");
-            
+
             int counter = 1;
             for (Work leaf : leafs) {
                 if (counter == position)
@@ -439,7 +531,7 @@ public interface Work extends Node {
             }
             return null;
         }
-        
+
         @Override
         public int countParts() {
             return (parts() == null) ? 0 : parts().size();
@@ -461,26 +553,27 @@ public interface Work extends Node {
         public AmberGraph getAmberGraph() {
             return this.asAmberVertex().getAmberGraph();
         }
-        
+
         /**
-         * Loads all of a work into the session including Pages with their Copies and Files
+         * Loads all of a work into the session including Pages with their
+         * Copies and Files
          */
         public void loadPagedWork() {
-            
+
             AmberVertex work = this.asAmberVertex();
             AmberGraph g = work.getAmberGraph();
-            
+
             AmberQuery query = g.newQuery((Long) work.getId());
-            query.branch(Lists.newArrayList(new String[] {"isPartOf"}), Direction.BOTH);
-            query.branch(Lists.newArrayList(new String[] {"isCopyOf"}), Direction.IN);
-            query.branch(Lists.newArrayList(new String[] {"isFileOf"}), Direction.IN);
+            query.branch(Lists.newArrayList(new String[] { "isPartOf" }), Direction.BOTH);
+            query.branch(Lists.newArrayList(new String[] { "isCopyOf" }), Direction.IN);
+            query.branch(Lists.newArrayList(new String[] { "isFileOf" }), Direction.IN);
             query.execute();
-            
+
             query = g.newQuery((Long) work.getId());
-            query.branch(Lists.newArrayList(new String[] {"existsOn"}), Direction.OUT);
+            query.branch(Lists.newArrayList(new String[] { "existsOn" }), Direction.OUT);
             query.execute();
         }
-        
+
         public List<Work> getPartsOf(List<String> subTypes) {
 
             AmberVertex work = this.asAmberVertex();
@@ -490,7 +583,8 @@ public interface Work extends Node {
             List<Work> works = new ArrayList<Work>();
             for (Edge e : partEdges) {
                 Vertex v = e.getVertex(Direction.OUT);
-                if (subTypes == null || subTypes.size() == 0 || subTypes.contains(v.getProperty("subType"))) {
+                if (subTypes == null || subTypes.size() == 0
+                        || subTypes.contains(v.getProperty("subType"))) {
                     works.add(this.g().frame(v, Work.class));
                 }
             }
@@ -506,7 +600,8 @@ public interface Work extends Node {
             List<Work> works = new ArrayList<Work>();
             for (Edge e : partEdges) {
                 Vertex v = e.getVertex(Direction.IN);
-                if (subTypes == null || subTypes.size() == 0 || subTypes.contains(v.getProperty("subType"))) {
+                if (subTypes == null || subTypes.size() == 0
+                        || subTypes.contains(v.getProperty("subType"))) {
                     works.add(this.g().frame(v, Work.class));
                 }
             }
@@ -514,11 +609,11 @@ public interface Work extends Node {
         }
 
         public List<Work> getPartsOf(String subType) {
-            return getPartsOf(Arrays.asList(new String[] {subType}));
+            return getPartsOf(Arrays.asList(new String[] { subType }));
         }
-        
+
         public List<Work> getExistsOn(String subType) {
-            return getExistsOn(Arrays.asList(new String[] {subType}));
+            return getExistsOn(Arrays.asList(new String[] { subType }));
         }
 
     }
