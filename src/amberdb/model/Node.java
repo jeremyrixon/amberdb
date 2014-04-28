acdb46a140cfe1517c3a5a77bed679dab4b3849a
JonathanShaw <anaximenes@gmail.com>
2014-04-28 14:04:23 +1000

    change aliases to alias.

--- src/amberdb/model/Node.java
+++ src/amberdb/model/Node.java
@@ -77,14 +77,14 @@ public interface Node extends VertexFrame {
     /**
      * This property is encoded as a JSON Array - You probably want to use getAlias to get this property
      */
-    @Property("aliases")
+    @Property("alias")
     public String getJSONAlias();
     
     /**
      * This property is encoded as a JSON Array - You probably want to use setAlias to set this property
      */
-    @Property("aliases")
-    public void setJSONAlias(String aliases);
+    @Property("alias")
+    public void setJSONAlias(String alias);
     
     /**
      * This method handles the JSON serialisation of the OtherNumbers Property
@@ -93,7 +93,7 @@ public interface Node extends VertexFrame {
      * @throws JsonParseException 
      */
     @JavaHandler
-    public void setAlias(List<String> aliases) throws JsonParseException, JsonMappingException, IOException;
+    public void setAlias(List<String> alias) throws JsonParseException, JsonMappingException, IOException;
     
     /**
      * This method handles the JSON deserialisation of the OtherNumbers Property
@@ -140,17 +140,17 @@ public interface Node extends VertexFrame {
         @Override
         public List<String> getAlias() throws JsonParseException, JsonMappingException, IOException {
             ObjectMapper mapper = new ObjectMapper();
-            String aliases = getJSONAlias();
-            if (aliases == null || aliases.isEmpty())
+            String alias = getJSONAlias();
+            if (alias == null || alias.isEmpty())
                 return new ArrayList<String>();
-            return mapper.readValue(aliases, new TypeReference<List<String>>() { } );
+            return mapper.readValue(alias, new TypeReference<List<String>>() { } );
             
         }
         
         @Override
-        public void setAlias( List<String>  aliases) throws JsonParseException, JsonMappingException, IOException {
+        public void setAlias( List<String>  alias) throws JsonParseException, JsonMappingException, IOException {
             ObjectMapper mapper = new ObjectMapper();
-            setJSONAlias(mapper.writeValueAsString(aliases));
+            setJSONAlias(mapper.writeValueAsString(alias));
         }
         
         
