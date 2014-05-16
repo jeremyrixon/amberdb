ec5ae2783a872105a167b4fb6c3304e3fed75642
Shuang Zhou <szhou@nla.gov.au>
2014-05-16 17:15:08 +1000

    minor update to lookups crud apis.

--- src/amberdb/sql/Lookups.java
+++ src/amberdb/sql/Lookups.java
@@ -137,12 +137,7 @@ public abstract class Lookups extends Tools {
         
         Long id = nextLookupId();
         if (id == null) id = 1L;
-        addLookupData(id, lu.getName(), code, lu.getValue());
-    }
-    
-    public synchronized void deleteLookup(ListLu lu) {
-        archiveDeletedLookup(lu.getId(), lu.getName(), lu.getCode());
-        markLookupDeleted(lu.getId(), lu.getName(), lu.getCode());
+        addLookupData(lu.getName(), code, lu.getValue());
     }
     
     public synchronized void updateLookup(ListLu lu) {
@@ -152,32 +147,32 @@ public abstract class Lookups extends Tools {
         ListLu persistedLu = findLookup(lu.getName(), code);
         if (persistedLu == null)
             throw new IllegalArgumentException(LuNotFoundErr);
-        
-        deleteLookup(lu);
-        addLookupData(lu.getId(), lu.getName(), code, lu.getValue());
+
+        updLookupData(lu.getName(), code, lu.getValue());
     }
     
     @SqlQuery("select max(id) + 1 from lookups")
     protected abstract Long nextLookupId();
     
-    @SqlUpdate("INSERT INTO lookups (id, name, code, value, deleted) VALUES"
-            + "(:id, :name, :code, :value, 'N')")
-    protected abstract void addLookupData(@Bind("id") Long id,
-                                              @Bind("name") String name,
-                                              @Bind("code") String code,
-                                              @Bind("value") String value);
+    @SqlUpdate("INSERT INTO lookups (name, code, value, deleted) VALUES"
+            + "(:name, :code, :value, 'N')")
+    protected abstract void addLookupData(@Bind("name") String name,
+                                          @Bind("code") String code,
+                                          @Bind("value") String value);
     
-    @SqlUpdate("UPDATE lookups SET deleted = 'D' "
-            + "WHERE id = :id and name = :name and (code is null or code = :code) and deleted = 'N'")
-    protected abstract void markLookupDeleted(@Bind("id") Long id,
-                                              @Bind("name") String name,
-                                              @Bind("code") String code);
+    @SqlUpdate("UPDATE lookups set value = :value "
+            + "where name = :name "
+            + "and code = :code "
+            + "and (deleted = 'N' or deleted is null)")
+    protected abstract void updLookupData(@Bind("name") String name,
+                                          @Bind("code") String code,
+                                          @Bind("value") String value);
     
-    @SqlUpdate("UPDATE lookups SET deleted = 'Y' "
-            + "WHERE id = :id and name = :name and (code = :code or value = :code) and deleted = 'D'")
-    protected abstract void archiveDeletedLookup(@Bind("id") Long id,
-                                                          @Bind("name") String name,
-                                                          @Bind("code") String code);
+    @SqlUpdate("UPDATE lookups SET deleted = 'D' WHERE id = :id")
+    public abstract void deleteLookup(@Bind("id") Long id);
+    
+    @SqlUpdate("UPDATE lookups SET deleted = 'N' WHERE id = :id")
+    public abstract void undeleteLookup(@Bind("id") Long id);
     
     @SqlUpdate("INSERT INTO lookups (id, name, code, value, deleted) VALUES"
             + "(:id, :name, :code, :value, :deleted)")
