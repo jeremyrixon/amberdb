283c18eac0ec141d4f99140d831afc85d63c6cd6
Aaron Muller <amuller@pc20276.nla.gov.au>
2014-07-31 15:01:01 +1000

    Add getPropertyKeySet method on base model classes.

---
+++ test/amberdb/model/NodeTest.java
@@ -0,0 +1,32 @@
+package amberdb.model;
+
+import amberdb.AmberSession;
+import com.google.common.collect.Sets;
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class NodeTest {
+
+    private static AmberSession db;
+
+    @Before
+    public void setup() throws IOException, InstantiationException {
+        db = new AmberSession();
+    }
+
+    @Test
+    public void propertySetHasAllProperties() {
+        Work w = db.addWork();
+        w.setBibLevel("bib");
+        assertEquals(w.getPropertyKeySet(), Sets.newHashSet("bibLevel", "type"));
+    }
+}
