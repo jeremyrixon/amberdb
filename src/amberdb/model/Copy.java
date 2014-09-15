ddba81a80ea28834a011074ce0ac484d8355e791
shaun <scoen@nla.gov.au>
2014-09-15 14:28:09 +1000

    removed unnecessary import statements

--- src/amberdb/model/Copy.java
+++ src/amberdb/model/Copy.java
@@ -6,11 +6,10 @@ import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import org.codehaus.jackson.JsonParseException;
