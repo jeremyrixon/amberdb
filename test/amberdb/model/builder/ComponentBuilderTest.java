135da576dff0fd2957f18ca63a5b570692c75c03
Shuang Zhou <szhou@nla.gov.au>
2014-10-16 11:43:10 +1100

    Adding EAD component unit test.

---
+++ test/amberdb/model/builder/ComponentBuilderTest.java
@@ -0,0 +1,45 @@
+package amberdb.model.builder;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ComponentBuilderTest {
+    static final Logger log = LoggerFactory.getLogger(ComponentBuilderTest.class);
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @Test
+    public void testDigitalObjectsMap() {
+        
+    }
+    
+    @Test
+    public void testPurgePreChecks() {
+        
+    }
+    
+    @Test
+    public void testPurgeComponents() {
+        
+    }
+    
+    @Test
+    public void testUpdateComponentData() {
+        
+    }
+    
+    @Test
+    public void testUpdateComponentPath() {
+        
+    }
+    
+    @Test
+    public void testMergeComponent() {
+        
+    }
+}
