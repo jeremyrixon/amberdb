47e7374e1dc7b1d15e2e2cb74fe21fb34647f1ce
Shuang Zhou <szhou@nla.gov.au>
2015-02-24 11:21:59 +1100

    adding lookup entries for file validation messages.

--- src/amberdb/sql/LookupsSchema.java
+++ src/amberdb/sql/LookupsSchema.java
@@ -955,6 +955,14 @@ public abstract class LookupsSchema {
     public abstract void seedEADUpdateReviewRequired();
     
     @SqlUpdate("INSERT INTO lookups (name, code, value) VALUES"
+    + "('dataValidationMsg', 'INVALID_FILE_NAME', 'The file ${filename} does not match the work''s PI.  Please rename your file to ${workObjId}.xml.'),"  
+    + "('dataValidationMsg', 'FILE_ALREADY_LOADED', 'File has already been uploaded for job ${jobId}.')," 
+    + "('dataValidationMsg', 'CANNOT_OPEN_FILE', 'Fail to access file ${filename}.')," 
+    + "('dataValidationMsg', 'MISSING_EAD_XML_NAMESPACE', 'Invalid EAD file, it is missing EAD xml namespace: ${xmlHeader}.')," 
+    + "('dataValidationMsg', 'VALID_FILE', '${filename} is a valid file.')")
+    public abstract void seedDataValidationMsgs();
+    
+    @SqlUpdate("INSERT INTO lookups (name, code, value) VALUES"
     + "('collection','nla.aus', 'Australian'),"
     + "('collection','nla.ms', 'Manuscript'),"
     + "('collection','nla.map', 'Map'),"
@@ -1058,6 +1066,7 @@ public abstract class LookupsSchema {
         createToolsMaterialTypeIdIndex();
         seedKeyCodeList();
         seedEADUpdateReviewRequired();
+        seedDataValidationMsgs();
         seedCollectionList();
         seedCopyTypeList();
         seedTiffMetaLuList();
