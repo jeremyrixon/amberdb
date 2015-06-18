56b8063ee330e30015bd68f189a4cc188c880b55
Shuang Zhou <szhou@nla.gov.au>
2015-06-18 11:14:43 +1000

    code update.

--- src/amberdb/util/OcrCheck.java
+++ src/amberdb/util/OcrCheck.java
@@ -17,56 +17,139 @@ import amberdb.enums.CopyRole;
 import amberdb.model.*;
 
 public class OcrCheck {
+    Work work;
+    Boolean hasImageCopy = null;
+    ImageFile image = null;
+    
+    /**
+     * OcrCheck constructor: construct an OcrCheck instance base on the input work.  
+     * @param work - a page containing image and ocr to be compared with or the parent work of the page to be checked.
+     */
+    public OcrCheck(Work work) {
+        if (work == null) {
+            throw new IllegalArgumentException("Input work is null.");
+        }
+        this.work = work;
+    }
+    
+    /**
+     * ocrOutOfBound: validate the input co-ords x, y, w, h are within the corresponding width and length of the image the ocr
+     * @param x - the start value in x axis
+     * @param y - the start value in y axis
+     * @param w - the width of the ocr bounding box
+     * @param h - the length of the ocr bounding box
+     * @return indicator whether the generated ocr content is outside the image width and length.
+     */
+    public boolean ocrOutOfBound(int x, int y, int w, int h) {
+        return ocrOutOfBound(getImage(), x, y, w, h);
+    }
+    
     /**
      * ocrOutOfBound: validate the generated ocr json in the jsonFile, and return true if the bounding box for the first paragraph
      *                is not within the corresponding width and length of the image the ocr is generated from. This method can be
      *                called before adding the jsonFile to the work's OCR_JSON_COPY.
-     * @param page - the page containing the image the ocr content is generated from.
      * @param jsonFile - the file containing generated ocr content in JSON format.
      * @return indicator whether the generated ocr content is outside the image width and length.
      * @throws UnsupportedEncodingException
      * @throws FileNotFoundException
      * @throws IOException
      */
-    public boolean ocrOutOfBound(Page page, java.io.File jsonFile) throws UnsupportedEncodingException, FileNotFoundException, IOException {
+    public boolean ocrOutOfBound(java.io.File jsonFile) throws UnsupportedEncodingException, FileNotFoundException, IOException {
         if (jsonFile == null) {
             throw new IllegalArgumentException("Input json file is null.");
         }
-        if (page == null) {
-            throw new IllegalArgumentException("Input page is null.");
-        }
-        return ocrOutOfBound(page, new FileInputStream(jsonFile));
+        return ocrOutOfBound(work, new FileInputStream(jsonFile));
     }
     
     /**
      * ocrOutOfBound: validate the generated ocr json in the work's ocr json copy, and return true if the bounding box for the first paragraph
      *                is not within the corresponding width and length of the image the ocr is generated from.  This method can be called to
      *                identify issues with existing generated ocr json.
-     * @param work - the page containing image and ocr to be compared or the parent work of the page to be checked.
      * @return indicator whether the generated ocr content is outside the image width and length.
      * @throws UnsupportedEncodingException
      * @throws IOException
      */
-    public boolean ocrOutOfBound(Work work) throws UnsupportedEncodingException, IOException {
+    public boolean ocrOutOfBound() throws UnsupportedEncodingException, IOException {
+        return ocrOutOfBound(this.work);
+    }
+    
+    private boolean ocrOutOfBound(Work work) throws UnsupportedEncodingException, IOException {
         return ocrOutOfBound(work, null);
+    }
+    
+    private ImageFile getImage() {
+        if (hasImageCopy != null) {
+            return image;
+        }
+        Copy pageImg = work.getCopy(CopyRole.CO_MASTER_COPY);
+        if (pageImg == null) {
+            pageImg = work.getCopy(CopyRole.MASTER_COPY);
+        }
+        if (pageImg != null) {
+            hasImageCopy = true;
+            image = pageImg.getImageFile();
+        } else {
+            hasImageCopy = false;
+            image = null;
+        }
+        return image;
+    }
+    
+    private boolean ocrOutOfBound(ImageFile image, int x, int y, int w, int h) {
+        if (x <= 0 || y <=0) {
+            throw new RuntimeException("Invalid ocr bounding box x and y from (x: " + x + ", y: " + y + ")");
+        }
+        
+        if (w <= 0 || h <=0) {
+            throw new RuntimeException("Invalid ocr bounding box width and length from (w: " + w + ", h: " + h + ")");
+        }
+
+        if (image == null) {
+            return false;
+        }
+        
+        Integer imageWidth = image.getImageWidth();
+        Integer imageLength = image.getImageLength();
+        Integer resolutionX = null;
+        String resolution = image.getResolution();
+        try {
+            if (resolution == null || resolution.isEmpty()) {
+                throw new RuntimeException("Missing image resolution for page " + work.getObjId());
+            }
+            resolutionX = new Integer(resolution.split("x")[0].replaceAll("[^0-9]+",""));
+            if (resolutionX <= 0) {
+                throw new RuntimeException("Invalid image resolution " +  resolution + " for page " + work.getObjId());
+            }
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Invalid image resolution " +  resolution + " for page " + work.getObjId());
+        }
+        
+        if (imageWidth == null || imageWidth <= 0) {
+            throw new RuntimeException("Missing image width for page " + work.getObjId());
+        }
+        
+        if (imageLength == null || imageLength <= 0) {
+            throw new RuntimeException("Missing image length for page " + work.getObjId());
+        }
+        
+        if (x > imageWidth || y > imageLength) {
+            return true;
+        }
+        
+        if (w > imageWidth || h > imageLength) {
+            return true;
+        }
+        return false;
     }
     
     private boolean ocrOutOfBound(Work work, InputStream in) throws UnsupportedEncodingException, IOException {
         if (work instanceof Page) {
             Copy ocrJson = work.getCopy(CopyRole.OCR_JSON_COPY);
-            Copy pageImg = work.getCopy(CopyRole.CO_MASTER_COPY);
-            if (pageImg == null) {
-                pageImg = work.getCopy(CopyRole.MASTER_COPY);
-            }
-            if (ocrJson == null || pageImg == null) {
+            if (ocrJson == null) {
                 return false;
             }
 
-            ImageFile image = pageImg.getImageFile();
-            if (image == null) {
-                return false;
-            }
-            
+            ImageFile image = getImage();            
             InputStream ocrStream = in;
             if (ocrStream == null) {
                 File ocrJsonFile = ocrJson.getFile();
@@ -74,30 +157,6 @@ public class OcrCheck {
                     return false;
                 }
                 ocrStream = ocrJsonFile.openStream();
-            }
-            
-            Integer imageWidth = image.getImageWidth();
-            Integer imageLength = image.getImageLength();
-            Integer resolutionX = null;
-            String resolution = image.getResolution();
-            try {
-                if (resolution == null || resolution.isEmpty()) {
-                    throw new RuntimeException("Missing image resolution for page " + work.getObjId());
-                }
-                resolutionX = new Integer(resolution.split("x")[0].replaceAll("[^0-9]+",""));
-                if (resolutionX <= 0) {
-                    throw new RuntimeException("Invalid image resolution " +  resolution + " for page " + work.getObjId());
-                }
-            } catch (NumberFormatException e) {
-                throw new RuntimeException("Invalid image resolution " +  resolution + " for page " + work.getObjId());
-            }
-            
-            if (imageWidth == null || imageWidth <= 0) {
-                throw new RuntimeException("Missing image width for page " + work.getObjId());
-            }
-            
-            if (imageLength == null || imageLength <= 0) {
-                throw new RuntimeException("Missing image length for page " + work.getObjId());
             }
             
             try (InputStreamReader isr = new InputStreamReader(new GZIPInputStream(ocrStream), "utf8")) {
@@ -112,22 +171,10 @@ public class OcrCheck {
                     
                     try {
                         Integer startX = Integer.parseInt(coOrds[0]);
-                        Integer startY = Integer.parseInt(coOrds[1]);
-                        if (startX <= 0 || startY <=0) {
-                            throw new RuntimeException("Invalid ocr bounding box x and y from (x: " + coOrds[0] + ", y: " + coOrds[1] + ")");
-                        }
-                        if (startX > imageWidth || startY > imageLength) {
-                            return true;
-                        }
-                        
+                        Integer startY = Integer.parseInt(coOrds[1]);                       
                         Integer widthBound = Integer.parseInt(coOrds[2]);
                         Integer lengthBound = Integer.parseInt(coOrds[3]);
-                        if (widthBound <= 0 || lengthBound <=0) {
-                            throw new RuntimeException("Invalid ocr bounding box width and length from (w: " + coOrds[2] + ", h: " + coOrds[3] + ")");
-                        }
-                        if (widthBound > imageWidth || lengthBound > imageLength) {
-                            return true;
-                        }
+                        return ocrOutOfBound(image, startX, startY, widthBound, lengthBound);
                     } catch (NumberFormatException e) {
                         throw new RuntimeException("Failed to parse ocr bounding box width and length from (w: " + coOrds[2] + ", h: " + coOrds[3] + ")");
                     }
