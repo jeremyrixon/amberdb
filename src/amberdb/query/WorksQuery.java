ec7944f1e129a6d1a24c9d6b32c3dce7e03f7f5e
shaun <scoen@nla.gov.au>
2015-10-02 14:04:33 +1000

    Added query to get a set of copies from a list of ids and load their corresponding works into the local graph

--- src/amberdb/query/WorksQuery.java
+++ src/amberdb/query/WorksQuery.java
@@ -1,9 +1,14 @@
 package amberdb.query;
 
 import amberdb.AmberSession;
+import amberdb.graph.AmberQuery;
+import amberdb.graph.BranchType;
+import amberdb.model.Copy;
 import amberdb.model.Work;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Vertex;
 
 public class WorksQuery {
@@ -15,5 +20,18 @@ public class WorksQuery {
             works.add(sess.getGraph().frame(v, Work.class));
         }
         return works;
+    }
+
+    public static List<Copy> getCopiesWithWorks(AmberSession sess, List<Long> copyIds) {
+        List<Copy> copies = new ArrayList<>();
+        AmberQuery query = sess.getAmberGraph().newQuery(copyIds);
+        query.branch(BranchType.BRANCH_FROM_ALL, new String[] { "isCopyOf" }, Direction.OUT);
+        List<Vertex> vertices = query.execute();
+        for (Vertex v : vertices) {
+            if (v.getProperty("type").equals("Copy")) {
+                copies.add(sess.getGraph().frame(v, Copy.class));
+            }
+        }
+        return copies;
     }
 }
