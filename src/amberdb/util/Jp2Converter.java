5b5b8a772747a2e4844532cce65db522a9bc5901
Shuang Zhou <szhou@nla.gov.au>
2016-01-06 11:46:15 +1100

    update tiff preconversion.

--- src/amberdb/util/Jp2Converter.java
+++ src/amberdb/util/Jp2Converter.java
@@ -107,8 +107,8 @@ public class Jp2Converter extends ExternalToolConverter {
         Path tmpFilePath = dstFilePath.getParent().resolve("tmp_" + dstFilePath.getFileName() + ".tif");
 
         try {
-            if ("image/jpeg".equals(imgInfo.mimeType)) {
-                // Jpeg - Convert to tiff
+            if (!"image/tiff".equals(imgInfo.mimeType)) {
+                // e.g. Jpeg - Convert to tiff
                 convertUncompress(srcFilePath, tmpFilePath);
             } else if (imgInfo.samplesPerPixel == 1 && imgInfo.bitsPerSample == 1) {
                 // Bitonal image - Convert to greyscale (8 bit depth)
