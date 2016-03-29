053558c949e11611f138e5e26e76c2f903164603
msun <msun@nla.gov.au>
2016-03-29 14:51:19 +1100

    fix catalogue report

--- src/amberdb/query/ObjectsQuery.java
+++ src/amberdb/query/ObjectsQuery.java
@@ -214,9 +214,10 @@ public class ObjectsQuery extends AmberQueryBase {
                 if (request.hasFilterPredicate()) {
                     vGraph.clear();
                     VersionedVertex vv = vGraph.getVertex(id);
-                    
-                    if (request.getFilterPredicate().apply(vv)) {
-                        modifiedObjects.put(id, transition);
+                    if (vv != null){
+                        if (request.getFilterPredicate().apply(vv)) {
+                            modifiedObjects.put(id, transition);
+                        }    
                     }
                 } else {
                     modifiedObjects.put(id, transition);
