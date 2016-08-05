2602cfda8daad3d4c947755507317083d4b3f55e
shaun <scoen@nla.gov.au>
2016-08-05 10:44:39 +1000

    attempt a rollback on a commit failure

--- src/amberdb/graph/AmberGraph.java
+++ src/amberdb/graph/AmberGraph.java
@@ -783,6 +783,11 @@ public class AmberGraph extends BaseGraph
                 log.debug("commit complete");
                 break retryLoop;
             } catch (RuntimeException e) {
+                try {
+                    dao.rollback();
+                } catch (Exception rollbackException) {
+                    log.error("Rollback failed: {}", rollbackException);
+                }
                 if (tryCount < retries) {
                     log.warn("AmberDb commit failed: Reason: {}\n" +
                             "Retry after {} milliseconds", e.getMessage(), backoff);
