623e6533f11669f333376aed62e1f2ffdb5a17c0
Jeremy Rixon <jrixon@nla.gov.au>
2016-09-05 16:25:59 +1000

    Fixup unit test breakages after merge

--- src/test/java/amberdb/AmberDbTest.java
+++ src/test/java/amberdb/AmberDbTest.java
@@ -58,7 +58,7 @@ public class AmberDbTest {
         Files.write(tmpFile, "Hello world".getBytes());
 
         
-        AmberDb adb = new AmberDb(JdbcConnectionPool.create("jdbc:h2:"+folder.getRoot()+"persist","per","per"), folder.getRoot().toPath());
+        AmberDb adb = new AmberDb(JdbcConnectionPool.create("jdbc:h2:"+folder.getRoot()+"persist;DATABASE_TO_UPPER=false","per","per"), folder.getRoot().toPath());
         
         Long sessId;
         Long bookId;
@@ -90,7 +90,7 @@ public class AmberDbTest {
         }
         // next, persist the session (by closing it) open a new one and get the contents
 
-        adb = new AmberDb(JdbcConnectionPool.create("jdbc:h2:"+folder.getRoot()+"persist","per","per"), folder.getRoot().toPath());
+        adb = new AmberDb(JdbcConnectionPool.create("jdbc:h2:"+folder.getRoot()+"persist;DATABASE_TO_UPPER=false","per","per"), folder.getRoot().toPath());
         try (AmberSession db = adb.begin()) {
 
             Work book2 = db.findWork(bookId);
@@ -109,7 +109,7 @@ public class AmberDbTest {
     @Test
     public void testSuspendResume() throws IOException {
         
-        AmberDb adb = new AmberDb(JdbcConnectionPool.create("jdbc:h2:"+folder.getRoot()+"persist","per","per"), folder.getRoot().toPath());
+        AmberDb adb = new AmberDb(JdbcConnectionPool.create("jdbc:h2:"+folder.getRoot()+"persist;DATABASE_TO_UPPER=false","per","per"), folder.getRoot().toPath());
         
         Long sessId;
         Long bookId;
@@ -123,7 +123,7 @@ public class AmberDbTest {
             sessId = db.suspend();
         }
 
-        AmberDb adb2 = new AmberDb(JdbcConnectionPool.create("jdbc:h2:"+folder.getRoot()+"persist","per","per"), folder.getRoot().toPath());
+        AmberDb adb2 = new AmberDb(JdbcConnectionPool.create("jdbc:h2:"+folder.getRoot()+"persist;DATABASE_TO_UPPER=false","per","per"), folder.getRoot().toPath());
         try (AmberSession db = adb2.resume(sessId)) {
             
             // now, can we retrieve the files ?
