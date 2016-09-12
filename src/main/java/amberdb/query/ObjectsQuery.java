d54dcbefe2b50d43847213168f0139c6af751093
Jeremy Rixon <jrixon@nla.gov.au>
2016-09-12 17:38:13 +1000

    Add field parentConstraint to work

--- src/main/java/amberdb/query/ObjectsQuery.java
+++ src/main/java/amberdb/query/ObjectsQuery.java
@@ -26,6 +26,8 @@ public class ObjectsQuery extends AmberQueryBase {
     }
 
     public ModifiedObjectsQueryResponse getArticlesForIndexing(ModifiedObjectsBetweenTransactionsQueryRequest request) {
+        long start = System.currentTimeMillis();
+
         LinkedHashMap<Long, String> modifiedObjects = new LinkedHashMap<Long, String>();
         LinkedHashMap<Long, String> reasons = new LinkedHashMap<Long, String>();
 
@@ -152,6 +154,8 @@ public class ObjectsQuery extends AmberQueryBase {
             boolean hasMore = (q.list().size() >= request.getTake());
 
             return new ModifiedObjectsQueryResponse(modifiedObjects, reasons, hasMore, hasMore ? request.getSkip() + request.getTake() : -1);
+        } finally {
+            System.err.println((System.currentTimeMillis() - start) / 1000f);
         }
     }
     
