3b0d4301c96b1ad6c7bd7befdc265ea7b5ddc2c4
Jeremy Rixon <jrixon@nla.gov.au>
2016-11-21 11:46:08 +1100

    Backout DDG-874 because it duplicates DDG-394

--- src/main/java/amberdb/util/WorkUtils.java
+++ src/main/java/amberdb/util/WorkUtils.java
@@ -7,7 +7,6 @@ import java.util.Set;
 
 import org.joda.time.LocalDate;
 
-import amberdb.enums.AccessAgreement;
 import amberdb.enums.AccessCondition;
 import amberdb.enums.CopyrightPolicy;
 import amberdb.enums.DepositType;
@@ -50,7 +49,6 @@ public class WorkUtils {
         boolean highResDownload;
         boolean allowOnsiteAccess = true;
         Set<String> constraint = new LinkedHashSet<String>();
-        AccessAgreement accessAgreement = AccessAgreement.BASIC_ACCESS;
         
         if (openAccess) {
             accessConditions = AccessCondition.UNRESTRICTED;
@@ -61,7 +59,6 @@ public class WorkUtils {
             internalAccessConditions = AccessCondition.OPEN;
             expiryDate = new LocalDate(2016, 4, 17).toDate();
             constraint.add("edeposit online access coming soon");
-            accessAgreement = AccessAgreement.OPEN_ACCESS_IMMEDIATELY;
             highResDownload = true;
         } else {
             accessConditions =  AccessCondition.METADATA_ONLY;
@@ -71,7 +68,6 @@ public class WorkUtils {
             if (restriction.matches("[0-9]+months")) {
                 expiryDate = new LocalDate().plusMonths(Integer.parseInt(restriction.replace("months", ""))).toDate();
                 constraint.add("edeposit online access after embargo");
-                accessAgreement = AccessAgreement.OPEN_ACCESS_EMBARGOED;
             } else {
                 if (DepositType.OnlineGovernment.toString().equalsIgnoreCase(work.getDepositType())) {
                     expiryDate = new LocalDate(work.getStartDate()).plusYears(50).toDate();
@@ -85,7 +81,6 @@ public class WorkUtils {
         work.setCopyrightPolicy(CopyrightPolicy.INCOPYRIGHT.code());
         work.setAccessConditions(accessConditions.code());
         work.setInternalAccessConditions(internalAccessConditions.code());
-        work.setAccessAgreement(accessAgreement.code());
         work.setExpiryDate(expiryDate);
         try { work.setConstraint(constraint); } catch (IOException e) { throw new RuntimeException(e); /* should never occur */ }
         
